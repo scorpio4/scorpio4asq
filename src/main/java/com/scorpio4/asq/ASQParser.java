f0f3f479383643d0f584616425c7854f7382f6c1
Lee Curtis <lee.curtis@me.com>
2014-09-10 17:11:40 +1000

    first commit

---
+++ src/main/java/com/scorpio4/asq/ASQParser.java
@@ -0,0 +1,17 @@
+package com.scorpio4.asq;
+/*
+ *
+
+ */
+
+/**
+ * Scorpio4 (c) 2013-2014
+ * Module: com.scorpio4.asq.core.parser
+ * @author lee
+ * Date  : 16/11/2013
+ * Time  : 12:02 AM
+ */
+public interface ASQParser {
+
+    public ASQ getASQ();
+}
